5b065cce97706d96c0e3931e6dc1e1c22f0cb8ae
mohamed oumoumad <oumoumad@gmail.com>
2023-03-12 15:05:47 +0100

    added url for editing blog and doc pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -52,15 +52,13 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/oumad/usdtweak-docs/tree/master/docs/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/oumad/usdtweak-docs/tree/master/blog/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
